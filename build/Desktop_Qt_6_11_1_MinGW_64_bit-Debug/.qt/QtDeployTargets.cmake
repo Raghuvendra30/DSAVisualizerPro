70c63aaed177e3dd2016c067bc162d6da2cbcfb0
Raghuvendra Pratap Singh <158501451+Raghuvendra30@users.noreply.github.com>
2026-06-13 19:22:15 +0530

    Delete build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_DSAVisualizerPro_FILE C:/Users/raghu/Desktop/DSAVisualizerPro/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/DSAVisualizerPro.exe)
-set(__QT_DEPLOY_TARGET_DSAVisualizerPro_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_DSAVisualizerPro_RUNTIME_DLLS C:/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/CMakeFiles/DSAVisualizerPro.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for DSAVisualizerPro.
-# This may be replaced when dependencies are built.
